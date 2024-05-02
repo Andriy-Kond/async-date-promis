d4f0e5abe0a9cf2e3adfb339e2fe39437f308c5a
Andrii <andrii.kondratovich@gmail.com>
2024-05-02 16:08:10 +0300

    solve: task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,38 @@
-const form = document.querySelector(".form");
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+const refs = { form: document.querySelector(".form") };
+
+refs.form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  const delay = Number(e.currentTarget.elements.delay.value);
+  const step = Number(e.currentTarget.elements.step.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay + step * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
